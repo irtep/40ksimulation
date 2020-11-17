dd09d394d56b275fb444503a5413b526a587ca56
irtep <rasanen.petri@gmail.com>
2020-11-17 21:23:16 +0200

    update1711202123

--- src/components/InfoBox.js
+++ src/components/InfoBox.js
@@ -2,6 +2,9 @@ import React, { Component } from "react";
 import DropDownComponent from './DropDownComponent.js';
 import { models } from '../data/unitsAndTerrain.js';
 import '../App.css';
+const modelActions = [
+  {name: 'move'}, {name: '-1 wound'}, {name: '+1 wound'}, {name: 'pivotLeft'}, {name: 'pivotRight'}
+];
 
 class InfoBox extends Component {
   constructor() {
@@ -27,7 +30,15 @@ class InfoBox extends Component {
         showThis = <DropDownComponent
         options = {models}
         newModelChosen = {this.sendToParent}
+        firstOption = 'choose a model'
         />;
+      break;
+      case 'modelChosen':
+      showThis = <DropDownComponent
+      options = {modelActions}
+      newModelChosen = {this.sendToParent}
+      firstOption = 'choose action'
+      />;
       break;
       case '':
         showThis = '';
